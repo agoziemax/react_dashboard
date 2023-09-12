78390466389be9211c557226fd2639bff72774e2
Agoziemax <agoziemax@yahoo.com>
2023-09-12 23:17:00 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, } from './pages';
 
 function App() {
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
