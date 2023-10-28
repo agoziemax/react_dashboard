4882d235e2ca61ae14d59f1176fe5bab2835ff23
Agoziemax <agoziemax@yahoo.com>
2023-10-28 22:11:00 +0100

    updated cart and chat components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,14 +11,14 @@ const Sidebar = () => {
   const {activeMenu, setActiveMenu} = useStateContext()
 
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-50 bg-sky-400 text-md m-2';
 
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
       {activeMenu && (
         <>
-          <div className='flex justify-between items-center'>
+          <div className='flex justify-between items-center  '>
             <Link
               to='/'
               onClick={() => setActiveMenu}
